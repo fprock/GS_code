08377a3bb3480c8f1b0c67c1e4e709d3436800ac
Chris <ChrisKnaappe@usehatchapp.com>
2021-03-25 20:16:23 -0400

    Logging and printing all baro data, Next timestamps -> checksums -> GPS(hoping theres a python lib for this)

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 # import serial
 import sys
-from classes import CompHumClass
 import time
 import argparse
 import struct
 
+
 def fakeSerial(inputFile):
-    return inputFile.readline()
+    return inputFile.read(2)
+
 
 rawPresFilePath = "logs/RawPresLog.txt"
 compPresFilePath = "logs/CompPresLog.txt"
@@ -14,118 +15,176 @@ rawTempFilePath = "logs/RawTempLog.txt"
 compTempFilePath = "logs/CompTempLog.txt"
 rawHumFilePath = "logs/RawHumLog.txt"
 compHumFilePath = "logs/CompTempLog.txt"
-baroMsgsFilePath = "baroMessages.dat"
+compAltFilePath = "logs/CompAltLog.txt"
+baroMsgsFilePath = "Testdata.log"
+dataLogFilePath = "logs/data.log"
 
 rawPresFile = open(rawPresFilePath, "w")
+rawPresFile.write("Received Raw Pressure Values\n")
 compPresFile = open(compPresFilePath, "w")
+compPresFile.write("Received calculated Pressure Values\n")
 rawTempFile = open(rawTempFilePath, "w")
+rawTempFile.write("Received Raw Temperature Values\n")
 compTempFile = open(compTempFilePath, "w")
+compTempFile.write("Received Raw Temperature Values\n")
 rawHumFile = open(rawHumFilePath, "w")
+rawHumFile.write("Received Raw Humidity Values\n")
 compHumFile = open(compHumFilePath, "w")
+compHumFile.write("Received calculated Humidity Values\n")
+compAltFile = open(compAltFilePath, "w")
+compAltFile.write("Received calculated Altitude Values\n")
+dataFile = open(dataLogFilePath, "w")
+
+
+def logData(dataType_count, data):
+    if dataType_count == 0:
+        print("Raw Pressure: " + str(data))
+        rawPresFile.write("Raw Pressure: " + str(data) + "\n")
+        dataFile.write("Raw Pressure: " + str(data) + "\n")
+    elif dataType_count == 1:
+        print("Raw Temperature: " + str(data))
+        rawTempFile.write("Raw Temperature: " + str(data) + "\n")
+        dataFile.write("Raw Temperature: " + str(data) + "\n")
+    elif dataType_count == 2:
+        print("Raw Humidity: " + str(data))
+        rawHumFile.write("Raw Humidity: " + str(data) + "\n")
+        dataFile.write("Raw Humidity: " + str(data) + "\n")
+    elif dataType_count == 3:
+        print("Calculated Pressure(Pa): " + str(data))
+        compPresFile.write("Calculated Pressure(Pa): " + str(data) + "\n")
+        dataFile.write("Calculated Pressure(Pa): " + str(data) + "\n")
+    elif dataType_count == 4:
+        print("Calculated Temperature(C): " + str(data))
+        compTempFile.write("Calculated Temperature(C): " + str(data) + "\n")
+        dataFile.write("Calculated Temperature(C): " + str(data) + "\n")
+    elif dataType_count == 5:
+        print("Calculated Humidity(%): " + str(data))
+        compHumFile.write("Calculated Humidity(%): " + str(data) + "\n")
+        dataFile.write("Calculated Humidity(%): " + str(data) + "\n")
+    elif dataType_count == 6:
+        print("Calculated Altitude: " + str(data))
+        compAltFile.write("Calculated Altitude(m): " + str(data) + "\n")
+        dataFile.write("Calculated Altitude(m): " + str(data) + "\n")
+    else:
+        print("IDK homie this shouldnt happen")
+        print(str(dataType_count))
+
 
 print("*BEGINNING PROGRAM*\n\n")
+dataFile.write("*BEGINNING PROGRAM*\n\n")
 parser = argparse.ArgumentParser(description="Parse bool")
 parser.add_argument("-d", '-development', default=False, action="store_true")
 args = parser.parse_args()
 
 
 def main():
-    firstFlag = False
-    secondFlag = False
-    baroFlag = False
-    messageFlag = False
+    state = "initial"
+    baro_state = "payloadLength"
     payLoadLenFlag = False
     payLoadLen = 0
     payLoadCount = 0
     dataCount = 0
+    dataType_count = 0
     convertingData = ""
     if args.d:
         print("*ENTERING DEVELOPMENT MODE*\n")
+        dataFile.write("*ENTERING DEVELOPMENT MODE*\n")
         baroMsgsFile = open(baroMsgsFilePath, "r")
+        print("READING FROM FILE\n")
+        dataFile.write("READING FROM FILE\n")
     else:
         ser = Serial('/dev/ttyUSB0', 9600)
         ser.flushInput()
         ser.flushOutput()
-    print("READING FROM FILE\n")
+        print("READING FROM SERIAL\n")
+        dataFile.write("READING FROM SERIAL\n")
+
     while True:
         if args.d:
             data_raw = fakeSerial(baroMsgsFile)
             if not data_raw:
                 break
         else:
-            print("READING FROM SERIAL")
             data_raw = str(ser.readline())
 
         data_raw = data_raw.strip()
-        if data_raw == "BB" and not firstFlag and not baroFlag and not messageFlag:
-            print("First start flag received")
-            firstFlag = True
-            baroFlag = False
-            messageFlag = False
-            payLoadLenFlag = False
+        if data_raw == "BB" and state == "initial":  # First starting byte
+            print("\nFirst FPROCK start flag received")
+            dataFile.write("\nFirst FPROCK start flag received\n")
+            state = "baroFirst"
             payLoadLen = 0
             payLoadCount = 0
-        elif data_raw == "AE" and firstFlag and not secondFlag and not baroFlag and not messageFlag:
-            print("Second start flag received")
-            secondFlag = True
-            firstFlag = False
-        elif secondFlag:
-            if data_raw == "00":
+        elif data_raw == "AE" and state == "baroFirst":  # Second stating byte
+            print("Second FPROCK start flag received")
+            dataFile.write("Second FPROCK start flag received\n")
+            state = "Class"
+        elif state == "Class":  # finding class
+            if data_raw == "00":  # message class
                 print("Message Class received")
-                messageFlag = True
-                firstFlag = False
-                secondFlag = False
-            elif data_raw == "01":
+                dataFile.write("Message Class received\n")
+                state = "messageClass"
+            elif data_raw == "01":  # baro class
                 print("Barometer Class received")
-                baroFlag = True
-                firstFlag = False
-                secondFlag = False
+                dataFile.write("Barometer Class received\n")
+                state = "baroClass"
             else:
                 print("Invalid class received discarding data until next start")
-                firstFlag = False
-                secondFlag = False
-        elif baroFlag:
-            if payLoadLenFlag:
+                dataFile.write("Invalid class received discarding data until next start\n")
+                state = "initial"
+        elif state == "baroClass":
+            if baro_state == "payload":
                 convertingData = data_raw + convertingData
                 dataCount = dataCount + 1
                 if dataCount == 4:
-                    print("Attempting to convert: " + convertingData)
                     data = struct.unpack('!f', bytes.fromhex(convertingData))[0]
-                    print(data)
+                    logData(dataType_count, data)
+                    dataType_count = dataType_count + 1
                     dataCount = 0
                     data = ""
                     convertingData = ""
                 payLoadCount = payLoadCount + 1
                 if payLoadCount == payLoadLen:
                     print("END OF PAYLOAD")
+                    dataFile.write("END OF PAYLOAD\n")
+                    state = "barochecksum"
+                    baro_state = "payloadLength"
+                    i = 0
                     payLoadCount = 0
-                    firstFlag = False
-                    secondFlag = False
-                    baroFlag = False
-                    messageFlag = False
-                    payLoadLenFlag = False
                     payLoadLen = 0
                     payLoadCount = 0
-            else:
+                    dataType_count = 0
+            elif baro_state == "payloadLength":
                 payLoadLenFlag = True
                 payLoadLen = int(data_raw, 16)
                 print("Barometer payload length is " + str(payLoadLen) + " bytes")
-        elif messageFlag:
+                dataFile.write("Barometer payload length is " + str(payLoadLen) + " bytes\n")
+                baro_state = "payload"
+        elif state == "messageClass":
             if payLoadLenFlag:
-                #message data conversion
+                # message data conversion
 
                 payLoadCount = payLoadCount + 1
                 if payLoadCount == payLoadLen:
                     print("END OF PAYLOAD")
+                    dataFile.write("END OF PAYLOAD\n")
                     payLoadCount = 0
             else:
                 payLoadLenFlag = True
                 payLoadLen = int(data_raw, 16)
                 print("Message payload length is " + str(payLoadLen) + " bytes")
+                dataFile.write("Message payload length is " + str(payLoadLen) + " bytes\n")
+        elif state == "barochecksum":
+
+            i = i + 1
+            print("Checksum #" + str(i) + ": " + data_raw)
+            if i == 2:
+                print("\n")
+                state = "initial"
         else:
-            print("ERROR: missing starting flag, discarding incoming data and waiting till next start flags")
-            firstFlag = False
-            secondFlag = False
+            print(
+                f"ERROR: missing starting flag, discarding incoming data({data_raw}) and waiting till next start flags")
+            dataFile.write("ERROR: missing starting flag, discarding incoming data({data_raw}) and waiting till next "
+                           "start flags\n")
 
     if args.d:
         baroMsgsFile.close()
