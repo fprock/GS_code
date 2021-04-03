8ec029e7a3b969a1d9aadd23e1023dc251bba9fe
chrispy <jones_christopher_b@protonmail.com>
2021-04-02 23:36:09 -0400

    pulled in master

--- importer.py
+++ importer.py
@@ -1,17 +1,16 @@
 import serial
 from multiprocessing import Pipe
 from codecs import *
-from datetime import *
+from datetime import datetime
+
 global receiver
 sender, receiver = Pipe()
 
 
 def importSerial(SerOrLog):
-    if not SerOrLog:
-        ser = serial.Serial('/dev/ttyUSB0', 9600)
-        ser.flushInput()
-        ser.flushOutput()
-
+    ser = serial.Serial('/dev/ttyUSB1', 9600)
+    ser.flushInput()
+    ser.flushOutput()
     byteFile = open("logs/ByteFile.txt", 'w')
     hexFile = open("logs/HexFile.txt", 'w')
     dataFile = open("Testdata.txt", "r")
